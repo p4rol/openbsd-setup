6a7f41876f4c73b973f11907bb5f14135e02e3e7
p4rol <133523706+p4rol@users.noreply.github.com>
2024-05-17 17:14:16 +0300

    Create reboot.sh
    basic crontab to reboot host at midnight

---
+++ reboot.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#
+# Script to reboot this host
+#
+# Crontab entry, runs at midnight every day
+# 0 0 * * * /root/dev/reboot.sh             #  Daily at Midnight (12:00 AM)
+#
+#
+#
+
+logger "Attempting to reboot the host"
+
+reboot

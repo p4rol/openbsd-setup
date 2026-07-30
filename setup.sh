36594fb7fe200b435571fc64c667c09b8b9d1303
p4rol <133523706+p4rol@users.noreply.github.com>
2026-07-30 15:25:20 +0300

    Update setup.sh
    Added the sensorsd.conf

--- setup.sh
+++ setup.sh
@@ -134,6 +134,9 @@ check_success "cp rc.local /etc/rc.local"
 cp gps_alert.sh /usr/local/bin/
 check_success "cp gps_alert.sh /usr/local/bin/"
 
+cp sensorsd.conf /etc/
+check_success "cp sensorsd.conf /etc/"
+
 # Call the function to add ldattach line to /etc/ttys
 add_ldattach_to_ttys
 
